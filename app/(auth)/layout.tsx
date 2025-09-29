c72f3734fc176d4f74e746e2c8e49beb1c90d515
Sandy Saputra <21221753145@smkassalaambandung.sch.id>
2025-09-29 19:41:24 +0700

    refactor(auth): simplify login, forgot password, verify otp and reset password pages with shared layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,30 @@
+import React from "react";
+import CampusCoinLogo from "@/components/custom/logo/campus-coin-logo";
+
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return <>
-    {children}
-  </>
+  return (
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <CampusCoinLogo size={200} />
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            {children}
+          </div>
+        </div>
+      </div>
+      <div className="bg-muted relative hidden lg:block">
+        <img
+          src="https://ui.shadcn.com/placeholder.svg"
+          alt="Auth Banner"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
+  )
 }
--- app/(auth)/login/reset-password/page.tsx
+++ app/(auth)/login/reset-password/page.tsx
@@ -1,9 +1,6 @@
 'use client';
 
-import React from 'react'
-
 import ResetPasswordForm from '@/components/custom/forms/reset-password-form';
-import CampusCoinLogo from '@/components/custom/logo/campus-coin-logo';
 
 export default function ResetPasswordPage({
     params,
@@ -12,25 +9,5 @@ export default function ResetPasswordPage({
     params: Promise<{ slug: string }>
     searchParams: Promise<{ [key: string]: string | string[] | undefined }>
 }) {
-    return (
-        <div className="grid min-h-svh lg:grid-cols-2">
-            <div className="flex flex-col gap-4 p-6 md:p-10">
-                <div className="flex justify-center gap-2 md:justify-start">
-                    <CampusCoinLogo size={200} />
-                </div>
-                <div className="flex flex-1 items-center justify-center">
-                    <div className="w-full max-w-xs">
-                        <ResetPasswordForm />
-                    </div>
-                </div>
-            </div>
-            <div className="bg-muted relative hidden lg:block">
-                <img
-                    src="https://ui.shadcn.com/placeholder.svg"
-                    alt="Image"
-                    className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-                />
-            </div>
-        </div>
-    )
+    return <ResetPasswordForm />
 }
