c215d8c77267116b5bf4c75244e7e81f9709e6b3
hanz-fr <raihanf787@gmail.com>
2025-10-10 20:53:45 +0700

    fix: navbar url and active color

--- components/custom/navbar/navbar.tsx
+++ components/custom/navbar/navbar.tsx
@@ -47,28 +47,28 @@ export default function CampusCoinNavbar() {
       </NavbarContent>
 
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem>
-          <Link color="foreground" href="/">
+        <NavbarItem isActive={(pathname == '/')}>
+          <Link color={(pathname == '/') ? 'primary' : 'foreground'} href="/">
             Home
           </Link>
         </NavbarItem>
-        <NavbarItem isActive>
-          <Link aria-current="page" href="/about">
+        <NavbarItem isActive={(pathname == '/about')}>
+          <Link color={(pathname == '/about') ? 'primary' : 'foreground'} aria-current="page" href="/about">
             About
           </Link>
         </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="/event">
+        <NavbarItem isActive={(pathname == '/event')}>
+          <Link color={(pathname == '/event') ? 'primary' : 'foreground'} href="/event">
             Event
           </Link>
         </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="/redeem">
+        <NavbarItem isActive={(pathname == '/redeem')}>
+          <Link color={(pathname == '/redeem') ? 'primary' : 'foreground'} href="/redeem">
             Redeem Store
           </Link>
         </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="/forum">
+        <NavbarItem isActive={(pathname == '/knowledge-sharing"')}>
+          <Link color={(pathname == '/knowledge-sharing') ? 'primary' : 'foreground'} href="/knowledge-sharing">
             Knowledge Sharing
           </Link>
         </NavbarItem>
