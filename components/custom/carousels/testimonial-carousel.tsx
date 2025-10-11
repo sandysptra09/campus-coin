d67ef73bc9e3b486616ae94a6571f59d3647d46a
Sandy Saputra <21221753145@smkassalaambandung.sch.id>
2025-10-11 19:16:52 +0700

    style: addjust padding in testimonial-carousel

--- components/custom/carousels/testimonial-carousel.tsx
+++ components/custom/carousels/testimonial-carousel.tsx
@@ -12,7 +12,7 @@ import TestimonialCard from '../cards/testimonial-card'
 export default function TestimonialCarousel() {
     return (
         <Carousel className="mt-6 w-full max-w-3xl mx-auto">
-            <CarouselContent>
+            <CarouselContent className='px-2 py-2 md:p-4 md:px-2 '>
                 {[...Array(3)].map((_, i) => (
                     <CarouselItem key={i}>
                         <TestimonialCard />
