df2bf0e9f8358f512b385035567729a31c8d12a7
Sandy Saputra <21221753145@smkassalaambandung.sch.id>
2025-10-11 17:16:36 +0700

    feat(home): add Event Highlight section with initial content

--- components/custom/sections/home/event-highlight.tsx
+++ components/custom/sections/home/event-highlight.tsx
@@ -2,8 +2,20 @@
 
 import React from 'react'
 
+import EventHighlightCarousel from '../../carousels/event-highlight-carousel';
+
 export default function EventHighlight() {
     return (
-        <div>event-highlight</div>
+        <section className='max-w-6xl mx-auto px-6 md:px-12 lg:px-20 py-20'>
+            <div className='text-center'>
+                <p className="text-md text-primary md:text-[29px] uppercase font-medium">
+                    Event Highlight
+                </p>
+                <h1 className="text-xl md:text-[40px] text-secondary font-medium mt-2 leading-snug">
+                    Stay updated with the latest activities and opportunities on campus.
+                </h1>
+                <EventHighlightCarousel />
+            </div>
+        </section>
     )
 }
