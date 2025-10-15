4786107f12e6fd4610c7109c37d4ed78ed9d32cd
hanz-fr <raihanf787@gmail.com>
2025-10-15 17:41:47 +0700

    feat: fetch redeem detail

--- app/(user)/redeem/[id]/page.tsx
+++ app/(user)/redeem/[id]/page.tsx
@@ -1,11 +1,17 @@
 import ProductDetail from "@/components/custom/sections/redeem/details/product-detail";
 import ReviewSection from "@/components/custom/sections/redeem/details/review-section";
 
-export default function RedeemPage() {
+export default async function RedeemPage({
+  params,
+}: {
+  params: Promise<{ id: string }>
+}) {
+  const { id } = await params;
+
   return (
     <>
       <main className="mx-auto max-w-6xl px-4 md:px-6 py-10">
-        <ProductDetail />
+        <ProductDetail id={id} />
         <hr className="my-10 border-[color:var(--color-border)]" />
         <ReviewSection />
       </main>
--- components/custom/sections/redeem/details/product-detail.tsx
+++ components/custom/sections/redeem/details/product-detail.tsx
@@ -7,12 +7,17 @@ import { Image } from "@heroui/react";
 import { MdPersonOutline, MdDateRange } from "react-icons/md";
 import { IoIosTimer } from "react-icons/io";
 import { IoLocationOutline } from "react-icons/io5";
+import redeemItemsAll from "@/data/redeem-items-all.json";
 
-export default function ProductDetail() {
+export default function ProductDetail(props: { id: string }) {
+  const item = redeemItemsAll.filter((e) => e.id == parseInt(props.id))[0];
+
+  console.log(item);
+
   return (
     <section className="grid gap-8 py-10 md:grid-cols-2">
       <Image
-        src={"https://img.freepik.com/free-photo/medium-shot-people-learning_23-2149300715.jpg"}
+        src={item.image}
         alt="reward"
         className="w-full h-full object-cover"
         width={1000}
@@ -21,25 +26,51 @@ export default function ProductDetail() {
       <div className="flex flex-col justify-between gap-10 my-6">
         <div className="flex flex-col justify-center">
           <h1 className="mb-3 text-lg md:text-2xl font-semibold leading-snug text-pretty">
-            101 Softskills Webinar
+            {item.title}
           </h1>
           <p className="max-w-prose text-base leading-relaxed text-muted-foreground">
-            Pelajari pentingnya komunikasi, kerja tim, dan etika profesional dalam dunia kerja modern.
+            {item.description}
           </p>
 
-          <p className="mt-4 text-base text-muted-foreground">
-            Webinar | Online | Professional
-          </p>
+          <p className="mt-4 text-base text-muted-foreground">{item.meta}</p>
 
           <ul className="mt-4 text-base text-muted-foreground">
-            <li className="flex gap-3"><MdPersonOutline className="my-auto"/> <span>Speaker: Dr. Amelia Sari</span></li>
-            <li className="flex gap-3"><MdDateRange className="my-auto"/> <span>Date: 2025-11-18</span></li>
-            <li className="flex gap-3"><IoIosTimer className="my-auto"/> <span>Duration: ~2 hours</span></li>
-            <li className="flex gap-3"><IoLocationOutline className="my-auto"/> <span>Google Meet</span></li>
+            {item.speaker ? (
+              <li className="flex gap-3">
+                <MdPersonOutline className="my-auto" />{" "}
+                <span>{item.speaker}</span>
+              </li>
+            ) : (
+              <></>
+            )}
+            {item.date ? (
+              <li className="flex gap-3">
+                <MdDateRange className="my-auto" />{" "}
+                <span>{item.date}</span>
+              </li>
+            ) : (
+              <></>
+            )}
+            {item.duration ? (
+              <li className="flex gap-3">
+                <IoIosTimer className="my-auto" />{" "}
+                <span>{item.duration}</span>
+              </li>
+            ) : (
+              <></>
+            )}
+            {item.location ? (
+              <li className="flex gap-3">
+                <IoLocationOutline className="my-auto" />{" "}
+                <span>{item.location}</span>
+              </li>
+            ) : (
+              <></>
+            )}
           </ul>
 
           <div className="mt-4 flex items-center gap-2 text-base">
-            <CoinBadge value={30} size={30} />
+            <CoinBadge value={item.coins} size={30} />
           </div>
         </div>
         <Button color="primary">Redeem</Button>
