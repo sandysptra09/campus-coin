f3a297861a249278c58bd298a44986b46a1f8f46
hanz-fr <raihanf787@gmail.com>
2025-10-15 13:41:19 +0700

    feat(knowledge sharing): add searchbar

--- app/(user)/knowledge-sharing/page.tsx
+++ app/(user)/knowledge-sharing/page.tsx
@@ -1,18 +1,20 @@
-import React from 'react'
+import React from "react";
 
-import Hero from '@/components/custom/sections/knowledge-sharing/hero'
-import TopPick from '@/components/custom/sections/knowledge-sharing/top-pick'
-import AllArticle from '@/components/custom/sections/knowledge-sharing/all-article'
+import Hero from "@/components/custom/sections/knowledge-sharing/hero";
+import TopPick from "@/components/custom/sections/knowledge-sharing/top-pick";
+import AllArticle from "@/components/custom/sections/knowledge-sharing/all-article";
+import SearchKnowledgeSharing from "@/components/custom/sections/knowledge-sharing/search-knowledge-sharing";
 
 export default function KnowledgeSharingPage({}: {
-    params: Promise<{ slug: string }>
-    searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
-    return (
-        <main className='w-full min-h-screen'>
-            <Hero />
-            <TopPick />
-            <AllArticle />
-        </main>
-    )
+  return (
+    <main className="w-full min-h-screen">
+      <Hero />
+      <SearchKnowledgeSharing />
+      <TopPick />
+      <AllArticle />
+    </main>
+  );
 }
